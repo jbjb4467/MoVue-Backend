24cd064049cfca970f4f065b834fab8cd71026c0
unknown <chechekk0@naver.com>
2020-11-22 14:17:57 +0900

    community model update

--- community/models.py
+++ community/models.py
@@ -3,6 +3,7 @@ from django.conf import settings
   
 
 class Article(models.Model):
+  title = models.CharField(max_length=100)
   content = models.TextField()
   user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
